d6f6d5cb65e7f621549f67aab94951b0bb2bcc5e
od3ng <noprianto@polinema.ac.id>
2023-01-18 05:10:36 +0700

    add L2 normalization

--- data-pre-processing/data-normalization.py
+++ data-pre-processing/data-normalization.py
@@ -7,7 +7,14 @@ names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
 df = read_csv(path, names=names)
 array = df.values
 
+# L1 Normalization
 data_normalizer = Normalizer(norm="l1").fit(array)
+data_normalized = data_normalizer.transform(array)
+set_printoptions(precision=2)
+print(data_normalized[0:3])
+
+# L2 Normalization
+data_normalizer = Normalizer(norm="l2").fit(array)
 data_normalized = data_normalizer.transform(array)
 set_printoptions(precision=2)
 print(data_normalized[0:3])
