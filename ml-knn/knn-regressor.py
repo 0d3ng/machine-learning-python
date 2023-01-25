9fa87b151f62b26ef2f55def4ab6b5a3db49d1d7
od3ng <noprianto@polinema.ac.id>
2023-01-25 21:39:32 +0700

    knn regressor

---
+++ ml-knn/knn-regressor.py
@@ -0,0 +1,18 @@
+ßimport numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsRegressor
+
+path = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+headernames = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
+
+dataset = pd.read_csv(path, names=headernames)
+print(dataset.head())
+array = dataset.values
+
+X = array[:, :2]
+y = array[:, 2]
+print(dataset.shape)
+
+classifier = KNeighborsRegressor(n_neighbors=8)
+classifier.fit(X, y)
+print("The MSE is:", format(np.power(y - classifier.predict(X), 2).mean()))
